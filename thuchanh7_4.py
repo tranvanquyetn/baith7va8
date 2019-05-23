b4443a907d26953efb89fe2de9cfa8fe3fba67e1
tranvanquyetn <48422559+tranvanquyetn@users.noreply.github.com>
2019-05-23 08:28:47 +0700

    Add files via upload

---
+++ thuchanh7_4.py
@@ -0,0 +1,8 @@
+def file_read_from_head(fname, nlines):
+    from itertools import islice
+    with open(fname) as f:
+        for line in islice(f, nlines):
+            print(line)
+
+
+file_read_from_head('test7_4.txt', 2)
